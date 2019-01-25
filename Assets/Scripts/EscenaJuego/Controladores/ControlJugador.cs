f43667dd2a8a7a60450a5c07c26f1a69b85b6174
David <davrodrila@gmail.com>
2019-01-25 19:29:34 +0100

    Implementado movimiento. Principio de generación de terreno

--- Assets/Scripts/EscenaJuego/Controladores/ControlJugador.cs
+++ Assets/Scripts/EscenaJuego/Controladores/ControlJugador.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class ControlJugador : ControladorBasico {
 
     Animator animator;
+
+    public GameObject posicionCentro, posicionIzquierda, posicionDerecha;
+
+
+    public EstadoJugador estado;
+    private Vector3 destino;
 	// Use this for initialization
 	void Start () {
         base.Start();
         animator = GetComponent<Animator>();
+        estado = EstadoJugador.Centro;
+        destino = gameObject.transform.position;
     }
 	
 	// Update is called once per frame
@@ -20,6 +28,44 @@ public class ControlJugador : ControladorBasico {
         if (Input.GetKeyDown(KeyCode.C))
         {
             animator.SetBool(Utils.ANIMATION_AGACHADO, true);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (estado == EstadoJugador.Derecha)
+            {
+                destino = posicionCentro.transform.position;
+                estado = EstadoJugador.Transicion;
+
+            } else if (estado == EstadoJugador.Centro)
+            {
+                destino = posicionIzquierda.transform.position;
+                estado = EstadoJugador.Transicion;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (estado == EstadoJugador.Izquierda)
+            {
+                destino = posicionCentro.transform.position;
+                estado = EstadoJugador.Transicion;
+            }
+            if (estado == EstadoJugador.Centro)
+            {
+                destino = posicionDerecha.transform.position;
+                estado = EstadoJugador.Transicion;
+            }
+        }
+        if (estado == EstadoJugador.Transicion)
+        {
+            if (transform.position.x > destino.x)
+            {
+                transform.position = new Vector3(transform.position.x - 1, transform.position.y);
+            }
+            else if (transform.position.x < destino.x)
+            {
+                transform.position = new Vector3(transform.position.x + 1, transform.position.y);
+            }
         }
  	}
 
@@ -41,5 +87,19 @@ public class ControlJugador : ControladorBasico {
     public override void juegoRestaurado()
     {
         Time.timeScale = 1f;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.name=="Central")
+        {
+            estado = EstadoJugador.Centro;
+        } else if (other.name=="Dcha")
+        {
+            estado = EstadoJugador.Derecha;
+        } else if (other.name == "Izq")
+        {
+            estado = EstadoJugador.Izquierda;
+        }
     }
 }
---
+++ Assets/Scripts/EscenaJuego/Controladores/ControladorMapa.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorMapa : ControladorBasico {
+
+    public GameObject elementoInicial;
+    public GameObject elementoSiguiente;
+    public Transform puntoInicial;
+
+    // Use this for initialization
+    void Start () {
+        base.Start();
+//                      GameObject              Position                        Rotation
+        var plataformaInicio = Instantiate(elementoInicial, puntoInicial.position, puntoInicial.rotation);
+        Transform destino = plataformaInicio.gameObject.transform.Find("PuntoAnclado");
+        Instantiate(elementoSiguiente, destino.position, destino.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void juegoEnPausa()
+    {
+        
+    }
+
+    public override void juegoRestaurado()
+    {
+        
+    }
+}
