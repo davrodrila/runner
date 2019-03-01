d7da1e8dc24f247446d9ccdd1018096266db98d1
David <davrodrila@gmail.com>
2019-03-01 18:32:40 +0100

    Scripts

---
+++ Assets/Scripts/EscenaJuego/Controladores/ColisionPlataforma.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionPlataforma : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var script_mapa = GameObject.Find("ControladorMapa").GetComponent<ControladorMapa>();
+        int numeroAleatorio = Random.Range(0, script_mapa.mapa.Count);
+        var elementoAleatorio = script_mapa.mapa[numeroAleatorio];
+        var puntoAnclaje = script_mapa.elementoActual.transform.GetChild(1);
+        script_mapa.elementoActual = Instantiate(elementoAleatorio, puntoAnclaje.position, puntoAnclaje.rotation);
+    }
+}
--- Assets/Scripts/EscenaJuego/Controladores/ControladorMapa.cs
+++ Assets/Scripts/EscenaJuego/Controladores/ControladorMapa.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class ControladorMapa : ControladorBasico {
 
     public GameObject elementoInicial;
-    public GameObject elementoSiguiente;
+    
     public Transform puntoInicial;
 
+    public GameObject elementoActual;
+
+    public List<GameObject> mapa;
     // Use this for initialization
     void Start () {
         base.Start();
-//                      GameObject              Position                        Rotation
+        mapa = new List<GameObject>();
+        encontrarPlataformas();
+        //                      GameObject              Position                        Rotation
         var plataformaInicio = Instantiate(elementoInicial, puntoInicial.position, puntoInicial.rotation);
         Transform destino = plataformaInicio.gameObject.transform.Find("PuntoAnclado");
-        Instantiate(elementoSiguiente, destino.position, destino.rotation);
+        int numeroAleatorio = Random.Range(0,mapa.Count);
+        Debug.Log(numeroAleatorio);
+        var elementoAleatorio = mapa[numeroAleatorio];
+        elementoActual = Instantiate(elementoAleatorio, destino.position, destino.rotation);
 	}
 	
+    void encontrarPlataformas()
+    {
+        var plataforma1 = GameObject.Find("Pasillo");
+        mapa.Add(plataforma1);
+        var plataforma2 = GameObject.Find("Pasillo_Izq");
+        mapa.Add(plataforma2);
+        var plataforma3 = GameObject.Find("Pasillo_Dcha");
+        mapa.Add(plataforma3);
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
