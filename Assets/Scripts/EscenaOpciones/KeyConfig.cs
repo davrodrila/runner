e7cc6d7c360d5e28939d4136b89fb730bb4c7240
David <davrodrila@gmail.com>
2019-03-29 18:44:51 +0100

    menu opciones
    Menu opciones: Script inicial

---
+++ Assets/Scripts/EscenaOpciones/KeyConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class KeyConfig : MonoBehaviour {
+    public Button botonSalto;
+    public Canvas panel;
+    private Boolean esperarATeclaDeSalto= false;
+
+	// Use this for initialization
+	void Start () {
+        botonSalto.onClick.AddListener(jumpRebind);
+        panel.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        botonSalto.GetComponentInChildren<Text>().text = ValorOpciones.JUMP_BUTTON.ToString();
+    }
+
+    void jumpRebind()
+    {
+        panel.enabled = true;
+        esperarATeclaDeSalto = true;
+    }
+
+    private void OnGUI()
+    {
+        var evento = Event.current;
+        if (evento.isKey && esperarATeclaDeSalto)
+        {
+            Debug.Log("Pulsado: " + evento.keyCode);
+            ValorOpciones.JUMP_BUTTON = evento.keyCode;
+            esperarATeclaDeSalto = false;
+            panel.enabled = false;
+        }
+    }
+    
+
+}
---
+++ Assets/Scripts/EscenaOpciones/ValorOpciones.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ValorOpciones {
+    public static KeyCode DEFAULT_JUMP_BUTTON = KeyCode.Space;
+
+    /**/
+    public static KeyCode JUMP_BUTTON = DEFAULT_JUMP_BUTTON;
+}
